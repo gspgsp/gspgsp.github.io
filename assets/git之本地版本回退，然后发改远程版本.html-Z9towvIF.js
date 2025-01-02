0b3f89b527b15355040ede3eebc73f33ceb72b74
guoshipeng <1554680230@qq.com>
2025-01-02 22:03:52 +0800

    deploy

---
+++ assets/git之本地版本回退，然后发改远程版本.html-Z9towvIF.js
@@ -0,0 +1,6 @@
+import{_ as t,c as n,h as i,o as s}from"./app-DaSvejJ2.js";const a={};function l(r,e){return s(),n("div",null,e[0]||(e[0]=[i(`<p>git之本地版本回退，然后发改远程版本:</p><div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text" data-title="text"><pre><code><span class="line">回滚到指定提交版本:</span>
+<span class="line">$ git reset --hard a14201ce5a455c3ad0aff1043772a5f8822449bc</span>
+<span class="line">  </span>
+<span class="line">回退之后，可以直接用本地版本覆盖远程:</span>
+<span class="line">$ git push -f origin dev</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2)]))}const d=t(a,[["render",l],["__file","git之本地版本回退，然后发改远程版本.html.vue"]]),p=JSON.parse('{"path":"/content/other/git/git%E4%B9%8B%E6%9C%AC%E5%9C%B0%E7%89%88%E6%9C%AC%E5%9B%9E%E9%80%80%EF%BC%8C%E7%84%B6%E5%90%8E%E5%8F%91%E6%94%B9%E8%BF%9C%E7%A8%8B%E7%89%88%E6%9C%AC.html","title":"git之本地版本回退，然后发改远程版本","lang":"en-US","frontmatter":{"sidebar":false,"title":"git之本地版本回退，然后发改远程版本","description":"git之本地版本回退，然后发改远程版本"},"headers":[],"git":{},"filePathRelative":"content/other/git/git之本地版本回退，然后发改远程版本.md"}');export{d as comp,p as data};
