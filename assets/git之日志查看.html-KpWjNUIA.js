bfc2fd0d6eb957bf9656d27dcfafc267c26aa41f
天行者 <2249875980@qq.com>
2025-09-10 23:54:24 +0800

    deploy

---
+++ assets/git之日志查看.html-KpWjNUIA.js
@@ -0,0 +1,10 @@
+import{_ as n,c as e,d as i,o as a}from"./app-De-UIJv6.js";const t={};function l(c,s){return a(),e("div",null,s[0]||(s[0]=[i(`<p>git之日志查看:</p><div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text" data-title="text"><pre><code><span class="line">git log</span>
+<span class="line"></span>
+<span class="line">这将显示所有提交历史(从最新提交 开始)</span>
+<span class="line">  </span>
+<span class="line">git log &lt;commit-hash&gt;</span>
+<span class="line">这将显示指定提交及其祖先的提交历史(从commit-hash开始到过去的提交)</span>
+<span class="line">  </span>
+<span class="line">git show &lt;commit-hash&gt;</span>
+<span class="line">这将显示指定提交的详细信息，包括提交的作者、日期、提交消息以及具体的修改内容</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2)]))}const r=n(t,[["render",l],["__file","git之日志查看.html.vue"]]),p=JSON.parse('{"path":"/content/other/git/git%E4%B9%8B%E6%97%A5%E5%BF%97%E6%9F%A5%E7%9C%8B.html","title":"git之日志查看","lang":"en-US","frontmatter":{"sidebar":false,"title":"git之日志查看","description":"git之日志查看"},"headers":[],"git":{},"filePathRelative":"content/other/git/git之日志查看.md"}');export{r as comp,p as data};
