72b8f85d77fa4add7663f26d4dc35bc10c4933b6
gspgsp <gspgsp@users.noreply.github.com>
2026-09-22 15:39:39 +0000

    deploy: gspgsp/vue-blog@a524a8e2d604bd9d3f06c46a784e85a1e8a837f4

---
+++ assets/swimlanesDiagram-OX7UBUGT-BVlWLL-s.js
@@ -0,0 +1,8 @@
+import{f as t,S as o}from"./flowDiagram-ZACVJCCL-hnAMk2yK.js";import"./chunk-AZZRMDJM-DuENfhUn.js";import"./chunk-VE5CLXGZ-B0k1q-oq.js";import"./chunk-4S3VB3X6-785hQcfx.js";import{m as e}from"./mermaid.esm.min-CGZ8UzYH.js";import"./chunk-HCQLZPVX-BJ65hoMn.js";import"./app-DRK-v7nq.js";var a=e(r=>`${t(r)}
+  .swimlane.cluster rect {
+    stroke: ${r.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=a,u=o({defaultLayout:"swimlane",styles:m});export{u as diagram};
